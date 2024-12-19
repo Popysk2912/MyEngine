9f65c3b5b74eeadf138752aa80193baaea40907c
ASNA2912 <sasha1vlad2931@gmail.com>
2024-12-19 05:13:30 +0200

    Коммит изменений в cmake-build-debug и cmake-build-release

---
+++ cmake-build-debug/_deps/glm-build/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Projects/New/C++/CMAKE/MyEngine/cmake-build-debug/_deps/glm-src/glm
+# Build directory: D:/Projects/New/C++/CMAKE/MyEngine/cmake-build-debug/_deps/glm-build/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
