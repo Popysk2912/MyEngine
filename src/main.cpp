f350580051448f364439adf4a5f48591f8e1de63
ASNA2912 <sasha1vlad2931@gmail.com>
2024-12-19 07:47:03 +0200

    Added static linked c/c++ libs in CMake.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include "Game.h"
 
-
 int main()
 {
     #ifdef NDEBUG
     std::cout << "Build type: Release" << std::endl;
     #else
     std::cout << "Build type: Debug" << std::endl;
-   #endif
+    #endif
 
     Game game(800, 600);
     game.run();
